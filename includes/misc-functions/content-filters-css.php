24f8531e47aa5b5bc96e5a6191ffc20d3d23cf09
Phil Johnston <support@mintplugins.com>
2015-05-30 18:08:07 -0400

    Make postgrid css Add to any existing css
    Filtered to mp_brick_additional_css

--- includes/misc-functions/content-filters-css.php
+++ includes/misc-functions/content-filters-css.php
@@ -58,7 +58,7 @@ function mp_stacks_brick_content_output_css_postgrid( $css_output, $post_id, $fi
 	
 	//Get CSS Output
 	
-	$css_output = '
+	$css_output .= '
 	#mp-brick-' . $post_id . ' .mp-stacks-grid-item{' . 
 			mp_core_css_line( 'color', $default_text_color ) . 
 			mp_core_css_line( 'width', round((100/$postgrid_per_row), 1), '%' ) . 
