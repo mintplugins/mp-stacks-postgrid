7ec7ea3abd50bab82c1c5eaa143d5ca4e31cd7a7
Phil Johnston <support@mintplugins.com>
2015-09-20 15:03:25 -0400

    Make PostGrid centred by default for Brick Alignment.

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -4,14 +4,30 @@
  *
  * @since 1.0.0
  *
- * @package    MP Stacks Features
+ * @package    MP Stacks + PostGrid
  * @subpackage Functions
  *
  * @copyright  Copyright (c) 2015, Mint Plugins
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @author     Philip Johnston
  */
- 
+
+/**
+ * Make PostGrid Content Type Centered by default
+ *
+ * @access   public
+ * @since    1.0.0
+ * @param    $centered_content_types array - An array containing a string for each content-type that should default to centered brick alignment.
+ * @param    $centered_content_types array - An array containing a string for each content-type that should default to centered brick alignment.
+ */
+function mp_stacks_postgrid_centered_by_default( $centered_content_types ){
+	
+	$centered_content_types['postgrid'] = 'postgrid';
+	
+	return $centered_content_types;
+	
+}
+add_filter( 'mp_stacks_centered_content_types', 'mp_stacks_postgrid_centered_by_default' );
  
 /**
  * Function which returns an array of font awesome icons
