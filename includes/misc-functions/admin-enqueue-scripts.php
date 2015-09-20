749bb0170f00c10429cc23f78757f0a00a1e4cab
Phil Johnston <support@mintplugins.com>
2015-09-20 14:34:40 -0400

    Include new admin enqueue file

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file contains the enqueue scripts function for the postgrid plugin
+ *
+ * @since 1.0.0
+ *
+ * @package    MP Stacks PostGrid
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2015, Mint Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
